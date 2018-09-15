fad4b845948d41cb9a0943d07eb004ac9e50ff62
Osman Ali <okali@uci.edu>
2018-09-14 22:49:49 -0700

    Setup backend

--- server/index.js
+++ server/index.js
@@ -2,22 +2,22 @@ const express = require('express');
 const fs = require('fs');
 const https = require('https');
 const path = require('path');
+const cors = require('cors');
 
 const app = express();
 
+app.use(cors());
 app.use((req, res, next) => {
 	res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With');
 	res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET, POST');
 	res.setHeader('Access-Control-Allow-Origin', '*');
 	next();
 });
+// app.use(express.static('../frontend'))
 
-app.use(express.static('../frontend'))
-
-app.get('/test', (req, res) => {
-	console.log('test');
-	res.json('hiii');
-});
+// Routes
+const spotify = require('./routes/spotify');
+app.use('/spotify', spotify );
 
 let options = {
 	key: fs.readFileSync(path.resolve(__dirname, '../conf/server.key')),
@@ -26,5 +26,5 @@ let options = {
 
 const PORT = 8081;
 https.createServer(options, app).listen(PORT, function () {
-	console.log('Extension Boilerplate service running on https', PORT);
+	console.log(`Server listening on port ${PORT}`);
 });
